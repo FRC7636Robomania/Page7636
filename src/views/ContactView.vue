6057a445dfe0bc402081af2c07947da39b49196c
Tim Huang <taxihuang030926@gmail.com>
2022-06-25 19:46:59 +0800

    * re-upload

---
+++ src/views/ContactView.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <div class="contact">
+    <Button
+      label="text"
+      class="p-button-prime"
+    />
+    <h1>This is a contact page</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.contact {
+  height: 120vh;
+}
+</style>
